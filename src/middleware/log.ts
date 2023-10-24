190408d48988bbd688c96e6876906b8e337cdab2
Ariel Barrios <ariel29111978@hotmail.com>
2023-10-24 16:04:03 -0300

    Agrega un middleware para conseguir el 'user-agent'

---
+++ src/middleware/log.ts
@@ -0,0 +1,13 @@
+import { NextFunction, Request, Response } from "express";
+
+const logMiddleware = (req: Request, res: Response, next: NextFunction) => {
+
+  const userAgent = req.headers["user-agent"]
+
+  console.log(userAgent)
+
+  next();
+  
+};
+
+export { logMiddleware};
