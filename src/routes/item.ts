2281e67b1148b11cb6349b62d4c23510153b2906
Ariel Barrios <ariel29111978@hotmail.com>
2023-10-24 16:06:11 -0300

    Agrega el middleware log

--- src/routes/item.ts
+++ src/routes/item.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { deleteItem, getItem, getItems, postItem, updateItem } from "../controllers/item";
+import { logMiddleware } from "../middleware/log";
 
 const router = Router();
 
 router.get("/", getItems);
-router.get("/:id", getItem);
+router.get("/:id", logMiddleware, getItem);
 
 router.put("/:id", updateItem);
 
