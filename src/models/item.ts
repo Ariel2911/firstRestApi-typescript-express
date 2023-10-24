f151eccd2203bc3b22437f8d017aa0110321c226
Ariel Barrios <ariel29111978@hotmail.com>
2023-10-24 08:42:07 -0300

    Agrega el model item

---
+++ src/models/item.ts
@@ -0,0 +1,31 @@
+import { Schema, model, Types, Model } from "mongoose";
+import { Car } from "../interfaces/car.interface";
+
+const itemSchema = new Schema<Car>(
+  {
+    color: {
+      type: String,
+    },
+    gas: {
+      type: String,
+      enum: ["gasoline", "electric"],
+    },
+    year: {
+      type: Number,
+    },
+    description: {
+      type: String,
+    },
+    price: {
+      type: Number,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false
+  }
+);
+
+const ItemModel = model('items', itemSchema);
+
+export default ItemModel;
