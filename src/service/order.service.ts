8d9567c1fa276dd1406447a2fea0c3e7bc8f9887
Ariel Barrios <ariel29111978@hotmail.com>
2023-10-26 16:51:30 -0300

    Agrega order.service.ts

---
+++ src/service/order.service.ts
@@ -0,0 +1,12 @@
+import ItemModel from "../models/item.model";
+
+const getOrders = async () => {
+
+  const responseItem = await ItemModel.find({});
+
+  return responseItem;
+  
+};
+
+export { getOrders };
+
